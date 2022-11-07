fd887a75391c9cce6e3c8c45cb5f06cf4a48c8a4
MagdaCodes <themsway123@gmail.com>
2022-11-07 11:09:32 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Magda Samuel and is {""}
-        <a
-          href="https://github.com/MagdaCodes/weather-app-react"
-          target="_blank"
-        >
-          Open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Magda Samuel and is {""}
+          <a
+            href="https://github.com/MagdaCodes/weather-app-react"
+            target="_blank"
+          >
+            Open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

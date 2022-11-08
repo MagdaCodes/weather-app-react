40432878a31b68a00f459c06787d5a54c9e783d4
MagdaCodes <themsway123@gmail.com>
2022-11-08 11:04:25 +0100

    Added Weather components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Weather from "./Weather.js";
+
 import "./App.css";
 
 export default function App() {
@@ -6,11 +8,13 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Magda Samuel and is {""}
           <a
             href="https://github.com/MagdaCodes/weather-app-react"
             target="_blank"
+            rel="noreferrer"
           >
             Open-sourced on GitHub
           </a>
